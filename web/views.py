6fd358defbd24d2b737628da3511888f98a079fb
root <root@inst-builder-debian-12-build-build-7srnl.us-central1-a.c.gce-image-builder.internal>
2024-09-29 20:02:33 +0000

    code test

--- web/views.py
+++ web/views.py
@@ -1,7 +1,7 @@
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Test Page")
+    return HttpResponse("Shiv Page")
 
 def about(request):
     return HttpResponse("Test About Page")
